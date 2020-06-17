7025b50e567eccd292cc7c1f22bcb70573f4d97e
Adrian-Montemayor <adrianmoncar@hotmail.com>
2020-06-17 01:05:18 -0500

    Multiple Linear Regression Notes

---
+++ Regression/MultipleLinearRegression/multiple_linear_regression.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd
+
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+
+
